07585835bea4b9f49cad636cf8b63b88579eb892
Mark Lee <mark.lee@capricasoftware.co.uk>
2022-10-09 19:04:49 +0100

    Add a basic full screen strategy for JavaFX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,11 @@
  * vlcj-javafx module.
  */
 module uk.co.caprica.vlcj.javafx {
+    requires java.desktop;
     requires javafx.graphics;
     requires uk.co.caprica.vlcj;
 
+    exports uk.co.caprica.vlcj.javafx.fullscreen;
     exports uk.co.caprica.vlcj.javafx.videosurface;
     exports uk.co.caprica.vlcj.javafx.view;
 }
